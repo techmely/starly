f1ddabfd6d280f680587e781c2121ce3beab59f0
harrytran998 <nhattq.coding@gmail.com>
2024-03-06 23:23:34 +0700

    feat(hono): add common context middleware

---
+++ packages/hono/src/common.ts
@@ -0,0 +1,18 @@
+import type { MiddlewareHandler } from "hono";
+import type { HonoEnv } from "./utils/environment";
+
+export function commonContextMiddleware(): MiddlewareHandler<HonoEnv> {
+  return (c, next) => {
+    c.set(
+      "location",
+      c.req.header("True-Client-IP") ??
+        c.req.header("CF-Connecting-IP") ??
+        // @ts-expect-error - the cf object will be there on cloudflare
+        c.req.raw?.cf?.colo ??
+        "",
+    );
+    c.set("userAgent", c.req.header("User-Agent"));
+
+    return next();
+  };
+}
