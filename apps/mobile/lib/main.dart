29db0f397ba90e8aee99cf00cb56ea1d1124fc1f
YuuDaRealest <164107532+YuuDaRealest@users.noreply.github.com>
2024-03-27 09:19:33 +0700

    Tml 17 (#44)
    Co-authored-by: Nousem is me <yusakithejoker@gmail.com>
    Co-authored-by: Harry Tran <nhattq.coding@gmail.com>

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> ∏
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
