efcad2194ebec8cf10c07767af0aa220d5ee73f2
harrytran998 <nhattq.coding@gmail.com>
2024-04-08 08:08:33 +0700

    feat: update query es gen rules

---
+++ packages/models/index.d.ts
@@ -0,0 +1,8 @@
+export * from "./gen/es/auth/auth.event.v1_pb.d";
+export * from "./gen/es/auth/auth.model.v1_pb.d";
+
+export * from "./gen/es/preferences/preferences.event.v1_pb.d";
+export * from "./gen/es/preferences/preferences.model.v1_pb.d";
+
+export * from "./gen/es/user/user.event.v1_pb.d";
+export * from "./gen/es/user/user.model.v1_pb.d";
---
+++ packages/models/index.js
@@ -0,0 +1,8 @@
+export * from "./gen/es/auth/v1/auth.event_pb";
+export * from "./gen/es/auth/v1/auth.model_pb";
+
+export * from "./gen/es/preferences/v1/preferences.event_pb";
+export * from "./gen/es/preferences/v1/preferences.model_pb";
+
+export * from "./gen/es/user/v1/user.event_pb";
+export * from "./gen/es/user/v1/user.model_pb";
