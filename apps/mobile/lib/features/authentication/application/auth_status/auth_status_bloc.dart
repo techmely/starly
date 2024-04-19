654188a044e5aa06258b3539e9805ffc7468725b
Nousem is me <yusakithejoker@gmail.com>
2024-04-19 23:47:13 +0700

    Add Authen Application Logic

---
+++ apps/mobile/lib/features/authentication/application/auth_status/auth_status_bloc.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../domain/use-cases/i_auth_facade.dart';
+
+part 'auth_status_event.dart';
+part 'auth_status_state.dart';
+part 'auth_status_bloc.freezed.dart';
+
+/// AuthStatusBloc manages the current user's auth status within the app.
+@injectable
+class AuthStatusBloc extends Bloc<AuthStatusEvent, AuthStatusState> {
+  final IAuthFacade _authFacade;
+
+  AuthStatusBloc(this._authFacade) : super(const AuthStatusState.initial()) {
+    on<AuthStatusEvent>((event, emit) async {
+      await event.map(
+        authCheckRequested: (e) async {
+          final authenticated = await _authFacade.checkAuthenticated();
+          emit(
+            authenticated
+                ? const AuthStatusState.authenticated()
+                : const AuthStatusState.unauthenticated(),
+          );
+        },
+        signedOut: (e) async {
+          await _authFacade.logout();
+          emit(const AuthStatusState.unauthenticated());
+        },
+      );
+    });
+  }
+}
