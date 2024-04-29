41561d2ed47560d618b50c6d7148214c945f16e8
harrytran998 <nhattq.coding@gmail.com>
2024-04-29 20:06:14 +0700

    TML-9999 fix: update code + configs

--- packages/design-system/src/icon/SvgUse.tsx
+++ packages/design-system/src/icon/SvgUse.tsx
@@ -1,4 +1,41 @@
-export const SvgUse: () => {
-  return <svg>
-  </svg>
+import React from "react";
+import type { FC, SVGAttributes } from "react";
+
+type SvgId =
+  | "brand-twitter"
+  | "brand-linkedin"
+  | "brand-facebook"
+  | "brand-telegram"
+  | "close-outline"
+  | "chevron-right-outline"
+  | "chevron-left-outline"
+  | "chevron-down-outline"
+  | "chevron-up-outline"
+  | "arrow-drop-down-rounded"
+  | "arrow-drop-up-rounded"
+  | "square-rounded"
+  | "check-circle-outline"
+  // In case you want to pass icon id dynamic
+  | (string & {});
+
+export interface SvgUseProps extends SVGAttributes<SVGElement> {
+  id: SvgId;
+  label?: string;
+  className?: string;
+  path?: string;
 }
+
+export const SvgUse: FC<SvgUseProps> = ({
+  id,
+  label = "Present Icon",
+  className,
+  path,
+  ...rest
+}) => {
+  const baseAssetPath = `${path || "/svg/all.svg"}#${id}`;
+  return (
+    <svg role="img" aria-label={label} className={className} {...rest}>
+      <use href={baseAssetPath} />
+    </svg>
+  );
+};
