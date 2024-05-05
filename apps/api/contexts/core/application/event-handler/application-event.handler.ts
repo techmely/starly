05db9b15ceb7906701cb9412fa7f5b6253bd59f7
harrytran998 <nhattq.coding@gmail.com>
2024-05-05 17:29:46 +0700

    chore: remove unnessary things

--- apps/api/contexts/core/application/event-handler/application-event.handler.ts
+++
@@ -1,5 +0,0 @@
-import type { DomainEvent } from "../../domain/events/domain-event.base";
-
-export interface ApplicationEventHandler<IDomainEvent extends DomainEvent, Response = unknown> {
-  handle(domainEvent: IDomainEvent): Promise<Response> | Promise<void>;
-}
--- apps/api/contexts/core/infra/mapper.base.ts
+++
@@ -1,7 +0,0 @@
-import type { Entity } from "../domain/entities/entity.base";
-
-export interface Mapper<DomainEntity extends Entity<unknown>, DbRecord, Response = unknown> {
-  toPersistence(entity: DomainEntity): DbRecord;
-  toDomain(record: DbRecord): DomainEntity;
-  toResponse(entity: DomainEntity): Response;
-}
