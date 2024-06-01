fcb7d3803428efb7e6e87dbbb4160e94db08d436
harrytran998 <nhattq.coding@gmail.com>
2024-06-01 18:05:38 +0700

    feat(models): add google protobuf models

--- packages/models/index.d.ts
+++ packages/models/index.d.ts
@@ -3,6 +3,11 @@ export * from "./gen/ts/account/v1/account.model";
 export * from "./gen/ts/account/v1/account.service";
 
 export * from "./gen/ts/common/common.model";
+export * from "./gen/ts/error/v1/response.error.model";
+
+export * from "./gen/ts/google/protobuf/any";
+export * from "./gen/ts/google/protobuf/empty";
+export * from "./gen/ts/google/protobuf/wrappers";
 
 export * from "./gen/ts/preferences/v1/preferences.event";
 export * from "./gen/ts/preferences/v1/preferences.model";
