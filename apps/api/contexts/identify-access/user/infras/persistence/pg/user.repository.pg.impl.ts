6e8905c575d31828ada146c8aa98760d451e6d30
harrytran998 <nhattq.coding@gmail.com>
2024-06-01 22:09:19 +0700

    chore(api): upgrade domain driven package version

--- apps/api/contexts/identify-access/user/infras/persistence/pg/user.repository.pg.impl.ts
+++ apps/api/contexts/identify-access/user/infras/persistence/pg/user.repository.pg.impl.ts
@@ -1,11 +1,18 @@
-import type { UserTable } from "@techmely/models";
+import type { UserModel } from "@techmely/models";
+import type { Kysely } from "kysely";
 import { PgRepositoryBase } from "#root/libs/db/repository-pg.base";
 import type { UserEntity } from "../../../domain/entities/user.entity";
 import type { IUserRepository } from "../../../domain/repo/user.repository";
+import type { UserMapper } from "../../mappers/user.mapper";
 
 export class UserPgRepository
-  extends PgRepositoryBase<UserEntity, UserTable, { users: UserTable }>
+  extends PgRepositoryBase<UserEntity, UserModel, { users: UserModel }>
   implements IUserRepository
 {
   protected tableName = "users";
+
+  // biome-ignore lint/complexity/noUselessConstructor: We need this
+  constructor(mapper: UserMapper, db: Kysely<any>) {
+    super(mapper, db);
+  }
 }
