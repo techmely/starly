03ea7afa64f7a7fa0cea7c89eec6f6613fc4ef56
harrytran998 <nhattq.coding@gmail.com>
2024-06-04 10:59:48 +0000

    feat(api): add user schema in domain

--- apps/api/contexts/identify-access/user/domain/user.schema.ts
+++ apps/api/contexts/identify-access/user/domain/user.schema.ts
@@ -1,1 +1,10 @@
+import { object, string } from "valibot";
+
 export const updateUserSchema = {};
+
+export const signInSchema = object({
+  email: string(),
+  password: string(),
+});
+
+export const signUpSchema = signInSchema;
