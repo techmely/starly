0d3db9cde21d8f7268691756abae5698975e8c1c
harrytran998 <nhattq.coding@gmail.com>
2024-06-04 11:05:50 +0000

    feat(api): update user api router

--- apps/api/contexts/identify-access/user/infras/http/routers/user.router.ts
+++ apps/api/contexts/identify-access/user/infras/http/routers/user.router.ts
@@ -1,19 +1,18 @@
 import type {} from "@techmely/domain-driven";
+import type { BaseResponse } from "@techmely/models";
 import { Hono } from "hono";
 import type { HonoEnv } from "#root/libs/hono/hono.types";
-import userService from "../../user.injection";
 import { useUserGuard } from "../middleware/user.guard";
-import type { BaseResponse } from "@techmely/models";
-import type { SuccessStatusCode } from "hono/utils/http-status";
 
 const router = new Hono<HonoEnv>();
 router.put("/");
-router.get("/me", useUserGuard(), async (c, next) => {
+router.get("/me", useUserGuard(), async (c) => {
   const user = c.get("user");
   if (user?.updatedAt) {
     c.header("Last-Modified", user.updatedAt);
-    return c.json<BaseResponse, SuccessStatusCode>({ data: user });
   }
+  const response: BaseResponse = { data: user };
+  return c.json(response);
 });
 router.put("/my_profile");
 router.put("/my_profile/avatar");
