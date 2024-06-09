93a57a9ba71cf6dca5c27d79498631c8f1851605
harrytran998 <nhattq.coding@gmail.com>
2024-06-09 22:18:57 +0700

    feat(api): add create user schema

--- apps/api/contexts/identify-access/user/domain/user.schema.ts
+++ apps/api/contexts/identify-access/user/domain/user.schema.ts
@@ -1,6 +1,21 @@
-import { object, string } from "valibot";
+import { AuthStrategy, UserStatus } from "@techmely/models";
+import { any, boolean, enum_, object, string } from "valibot";
 
-export const updateUserSchema = {};
+export const createUserSchema = object({
+  email: string(),
+  nickname: string(),
+  status: enum_(UserStatus),
+  isEmailVerified: boolean(),
+  name: string(),
+  avatarUrl: string(),
+  firebaseUserId: string(),
+  authStrategy: enum_(AuthStrategy),
+  openPlatform: string(),
+  utmCampaign: string(),
+  utmMedium: string(),
+  utmSource: string(),
+  metadata: any(),
+});
 
 export const signInSchema = object({
   email: string(),
