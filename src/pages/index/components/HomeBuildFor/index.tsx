22253ecbdbc0636fe3df09a01f0bb08511dc952a
Harry Tran <nhattq.coding@gmail.com>
2024-08-26 10:46:58 +0700

    STL-9999 chore: upgrade deps (#129)

--- src/pages/index/components/HomeBuildFor/index.tsx
+++ src/pages/index/components/HomeBuildFor/index.tsx
@@ -19,28 +19,28 @@ const HomeBuildFor: FC<Props> = (props) => {
         </h2>
       </div>
       <ul className="starly-container relative py-8 h-[210px]">
-        <li className="absolute z-1 top-8 left-20 transform rotate-12">
-          <ProLabel title={t.homeDeveloper()} avatar={HomeDeveloper} className="bg-blue-200" />
+        <li className="absolute z-[1] top-10 left-32 transform rotate-12">
+          <ProLabel title={t.homeDeveloper()} avatar={HomeDeveloper} className="bg-blue-100" />
         </li>
-        <li className="absolute z-0 top-20 left-48 bg-gray-500 py-2 px-4 rounded-3xl">
+        <li className="absolute z-0 top-20 left-60 bg-[#7D7D7D] py-3 px-4 rounded-3xl">
           <p className="~text-xs/sm text-center text-white">{t.homeDeveloperBenif()}</p>
         </li>
-        <li className="absolute top-8 right-20">
-          <ProLabel title={t.homeManager()} avatar={HomeManager} className="bg-red-200" />
+        <li className="absolute top-4 right-8 z-[1] rotate-[15deg] transform-gpu">
+          <ProLabel title={t.homeManager()} avatar={HomeManager} className="bg-red-100" />
         </li>
-        <li className="absolute top-20 right-24 bg-gray-500 py-2 px-4 rounded-3xl">
+        <li className="absolute top-10 right-32 bg-[#7D7D7D] py-3 px-4 rounded-3xl">
           <p className="~text-xs/sm text-center text-white">{t.homeManagerBenif()}</p>
         </li>
-        <li className="absolute bottom-0 left-0">
-          <ProLabel title={t.homeCreator()} avatar={HomeCreator} className="bg-primary-200" />
+        <li className="absolute -bottom-4 left-28 z-[1] rotate-[-10deg] transform-gpu">
+          <ProLabel title={t.homeCreator()} avatar={HomeCreator} className="bg-primary-100" />
         </li>
-        <li className="absolute -bottom-10 left-0 bg-gray-500 py-2 px-4 rounded-3xl">
+        <li className="absolute -bottom-10 left-48 bg-[#7D7D7D] py-3 px-4 rounded-3xl">
           <p className="~text-xs/sm text-center text-white">{t.homeCreatorBenif()}</p>
         </li>
-        <li className="absolute bottom-0 right-0">
-          <ProLabel title={t.homeDesigner()} avatar={HomeDesigner} className="bg-purple-200" />
+        <li className="absolute z-[1] bottom-8 right-[28rem] rotate-[10deg] transform-gpu">
+          <ProLabel title={t.homeDesigner()} avatar={HomeDesigner} className="bg-purple-100" />
         </li>
-        <li className="absolute -bottom-10 right-0 bg-gray-500 py-2 px-4 rounded-3xl">
+        <li className="absolute bottom-4 right-24 bg-[#7D7D7D] py-3 px-4 rounded-3xl">
           <p className="~text-xs/sm text-center text-white">{t.homeDesignerBenif()}</p>
         </li>
       </ul>
