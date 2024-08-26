b191adce16b7e36318e62e0fec88713fbd371ce9
harrytran998 <nhattq.coding@gmail.com>
2024-08-26 10:46:24 +0700

    STL-9999 feat: update design for pro

--- src/pages/index/components/HomeBuildFor/ProLabel.tsx
+++ src/pages/index/components/HomeBuildFor/ProLabel.tsx
@@ -10,7 +10,7 @@ type Props = {
 const ProLabel: FC<Props> = ({ avatar, title, className }) => {
   return (
     <div
-      className={clsx("flex items-center justify-center gap-4 px-2.5 py-1 rounded-3xl", className)}
+      className={clsx("flex items-center justify-center gap-2 px-3 py-2 rounded-3xl", className)}
     >
       <div className="w-8 h-8 rounded-full">
         <img src={avatar} loading="lazy" alt={`avatar ${title}`} />
